65233a71f6ee74f8d3a130f1a903fd25210ea3fd
abused_master <techperson71@gmail.com>
2019-04-01 23:55:48 -0400

    Added isEnergyProvider & isEnergyReceiver

--- src/main/java/nerdhub/cardinalenergy/api/IEnergyHandler.java
+++ src/main/java/nerdhub/cardinalenergy/api/IEnergyHandler.java
@@ -9,12 +9,31 @@ import net.minecraft.util.math.Direction;
 public interface IEnergyHandler {
 
     /**
-     * Is used to define weather or not cables and energy connections can connect at a given side and provide energy
+     * Is used to define weather or not cables and energy connections can connect at a given side
      * Defaults to true unless overridden
      * @param direction - The direction to attempt connection, could be null
      * @return - True if a connection is successful
      */
     default boolean canConnectEnergy(Direction direction) {
+        return true;
+    }
+
+    /**
+     * Is used to define weather this block should not receive energy but provide it
+     * Default set to false to always receive
+     * @param direction - The direction to attempt sending, could be null
+     * @return - True if it provides energy, false if a receiver
+     */
+    default boolean isEnergyProvider(Direction direction) {
+        return false;
+    }
+
+    /**
+     * Is used to define weather this block can receive energy
+     * @param direction - The direction to attempt receiving, could be null
+     * @return - True if it can receive energy
+     */
+    default boolean isEnergyReceiver(Direction direction) {
         return true;
     }
 
