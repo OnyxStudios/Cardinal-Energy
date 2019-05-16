2a6dce32122dbbe3cdffc80b9d5429c9c54aabc0
abused_master <techperson71@gmail.com>
2019-05-16 13:04:26 -0400

    Added Cardinal Components support

--- src/main/java/nerdhub/cardinalenergy/DefaultTypes.java
+++ src/main/java/nerdhub/cardinalenergy/DefaultTypes.java
@@ -1,9 +1,15 @@
 package nerdhub.cardinalenergy;
 
+import nerdhub.cardinal.components.api.ComponentRegistry;
+import nerdhub.cardinal.components.api.ComponentType;
 import nerdhub.cardinalenergy.api.EnergyType;
+import nerdhub.cardinalenergy.api.IEnergyItemStorage;
+import nerdhub.cardinalenergy.api.IEnergyStorage;
 import nerdhub.cardinalenergy.impl.EnergyStorage;
 
 public class DefaultTypes {
 
+    public static final ComponentType<IEnergyStorage> CARDINAL_ENERGY_COMPONENT = ComponentRegistry.getOrCreate(IEnergyStorage.class);
+    public static final ComponentType<IEnergyItemStorage> CARDINAL_ENERGY_ITEM_COMPONENT = ComponentRegistry.getOrCreate(IEnergyItemStorage.class);
     public static EnergyType CARDINAL_ENERGY = new EnergyStorage();
 }
--- src/main/java/nerdhub/cardinalenergy/impl/example/BlockEntityEnergyImpl.java
+++ src/main/java/nerdhub/cardinalenergy/impl/example/BlockEntityEnergyImpl.java
@@ -1,17 +1,25 @@
 package nerdhub.cardinalenergy.impl.example;
 
+import nerdhub.cardinal.components.api.BlockComponentProvider;
+import nerdhub.cardinal.components.api.ComponentType;
+import nerdhub.cardinalenergy.DefaultTypes;
 import nerdhub.cardinalenergy.api.EnergyType;
 import nerdhub.cardinalenergy.api.IEnergyHandler;
 import nerdhub.cardinalenergy.impl.EnergyStorage;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
+import net.minecraft.world.BlockView;
+
+import java.util.Collections;
+import java.util.Set;
 
 /**
  * An example impl of {@link IEnergyHandler}
  */
-public class BlockEntityEnergyImpl extends BlockEntity implements IEnergyHandler {
+public class BlockEntityEnergyImpl extends BlockEntity implements IEnergyHandler, BlockComponentProvider {
 
     //Create an EnergyStorage instance that stores 10,000 energy
     public EnergyStorage storage = new EnergyStorage(10000);
@@ -37,7 +45,24 @@ public class BlockEntityEnergyImpl extends BlockEntity implements IEnergyHandler
 
     @Override
     public EnergyType getEnergyStorage(Direction direction) {
-        //Return the direction regardless of the direction
+        //Return the EnergyType based on direction, here we discard it
         return this.storage;
+    }
+
+    //Example with components
+    //IEnergyHandler is not technically needed if using Components
+    @Override
+    public <T> boolean hasComponent(BlockView blockView, BlockPos pos, ComponentType<T> type, Direction side) {
+        return type == DefaultTypes.CARDINAL_ENERGY_COMPONENT;
+    }
+
+    @Override
+    public <T> T getComponent(BlockView blockView, BlockPos pos, ComponentType<T> type, Direction side) {
+        return (T) storage;
+    }
+
+    @Override
+    public Set<ComponentType<?>> getComponentTypes(BlockView blockView, BlockPos pos, Direction side) {
+        return Collections.singleton(DefaultTypes.CARDINAL_ENERGY_COMPONENT);
     }
 }
